0c48222ad4a12bb5dadcd0fa8c1e1e51083a6601
Darsh <darsh2987@gmail.com>
2020-09-11 15:12:40 +0100

    functionality for updateWrongLetters function and play again function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const wordEl = document.querySelector("#word");
 const wrongLettersEl = document.querySelector("#wrong-letters");
-const playAgainBtn = document.querySelector("#play-again");
+const playAgainBtn = document.querySelector("#play-button");
 const popup = document.querySelector("#popup-container");
 const notification = document.querySelector("#notification-container");
 const finalMessage = document.querySelector("#final-message");
@@ -8,13 +8,14 @@ const finalMessage = document.querySelector("#final-message");
 const figureParts = document.querySelectorAll(".figure-part");
 
 // Array of words
-const words = ["friends", "heroes", "angel", "titans"];
+const words = ["friends", "heroes", "angel", "titans", "gotham", "suits"];
 
 // Generate random word from the words array
 let randomWord = words[Math.floor(Math.random() * words.length)];
 
-const correctLetters = []; // Users Guess
-const wrongLetters = []; // Users Guess
+let correctLetters = []; // Users Guess
+let wrongLetters = []; // Users Guess
+let gameOver = false;
 
 /* Show hidden word - 
 set wordEl's inner html to the random word -> 
@@ -43,6 +44,7 @@ function displayWord() {
   if (innerWord === randomWord) {
     finalMessage.innerText = "Congratulation You Won!";
     popup.style.display = "flex";
+    gameOver = true;
   }
 }
 
@@ -55,11 +57,44 @@ function showNotification() {
   }, 3000);
 }
 
-// Update the wrong letters DOM element
-function updateWrongLettersEl() {}
+/* Update the wrong letters DOM element -
+If wrong letters array has item then show the "Wrong Letters" text ->
+.map the wrong letters array to display each letter ->
+*/
+function updateWrongLettersEl() {
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? "<p>Wrong Letters<p>" : ""}
+    ${wrongLetters.map((letter) => `<span>${" " + letter}<span>`)}
+  `;
+
+  /* Show the figure part when a "wrong letter" has been guessed -
+  iterate through the figure part array ->
+  Get the length of wrong letters array and store in errors variable ->
+  check the "forEach index" is less then the errors variable, if so then set display of "part" to block
+  */
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  /* Check if lost the game -> 
+  show final message -> 
+  set gameOver variable to true which will not execute the code block with the keydown event funtion
+  */
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Sorry you lost";
+    popup.style.display = "flex";
+    gameOver = true;
+  }
+}
 
 /* Event for when a letter is pressed -
 Event Listener keydown for when a key is pressed ->
+following code only runs when "gameOver" is true (!gameOver = true)
 store the pressed key letter into a variable ->
 check if pressed letter is in the random word and then check if it doesn't exist within the correctLetters array ->
 if letter in not in the correctLetters array then push ->
@@ -70,24 +105,43 @@ if letter is not in the wrongLetter array then push ->
 call function updateWrongLettersEl
 */
 window.addEventListener("keydown", (e) => {
-  const letter = e.key;
-  console.log(letter);
-  if (randomWord.includes(letter)) {
-    if (!correctLetters.includes(letter)) {
-      correctLetters.push(letter);
-      displayWord();
+  if (!gameOver) {
+    const letter = e.key;
+    if (randomWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
     } else {
-      showNotification();
-    }
-  } else {
-    if (!wrongLetters.includes(letter)) {
-      wrongLetters.push(letter);
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
 
-      updateWrongLettersEl();
-    } else {
-      showNotification();
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
     }
   }
+});
+
+/* Play again button -
+reset everything
+*/
+
+playAgainBtn.addEventListener("click", () => {
+  correctLetters = [];
+  wrongLetters = [];
+
+  popup.style.display = "none";
+
+  randomWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+  updateWrongLettersEl();
+
+  gameOver = false;
 });
 
 displayWord();
