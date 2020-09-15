d3fce7260afa2c5a508a83248963669b8daddd64
Darsh <darsh2987@gmail.com>
2020-09-15 12:13:16 +0100

    changed window load function to arrow function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   // Global variables
   const popup = document.querySelector("#popup-container");
   const finalMessage = document.querySelector("#final-message");
